1bfb7cdd20e0859a022e91aef229f465c82a34eb
stampa3d <76432461+stampa3d@users.noreply.github.com>
2021-01-19 14:54:08 +0100

    GTRfirmware
    perfetta aggiornato a ultimo bugfix

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -70,7 +70,7 @@
 // @section info
 
 // Author info of this build printed to the host during boot and M115
-#define STRING_CONFIG_H_AUTHOR "(Io, Sirius 3d reality)" // Who made the changes.
+#define STRING_CONFIG_H_AUTHOR "(Stampa3d Reality, Sirius 3d con GTR perfetta)" // Who made the changes.
 //#define CUSTOM_VERSION_FILE Version.h // Path from the root directory (no quotes)
 
 /**
